38560a49fc9bafd994a1fecc24e314c1d580b40b
nasyaoy <122662286+nasyaoy@users.noreply.github.com>
2025-05-23 18:24:16 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Главная страница</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <header class="header">
+            <div class="nav">
+                <ul class="nav__list">
+                    <li class="nav__item"></li>
+                    <li class="nav__item"></li>
+                    <li class="nav__item"></li>
+                </ul>
+            </div>
+        </header>
+    </div>
+</body>
+
+</html>
